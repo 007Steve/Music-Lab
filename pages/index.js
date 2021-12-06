fcfe3e5df7c550393499f3b1a7a1db47af53b7fd
007steve <stephen.plummer1.sp@gmail.com>
2021-12-06 09:24:44 -0500

    clean up login page and added control inputs

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import CheckIcon from "@material-ui/icons/Check";
+import Link from "next/link";
+import Logo from "../components/Logo";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Music Lab</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <form className={styles.r} onSubmit>
+        <Logo />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.signUp__inputContainer}>
+          <input
+            className={styles.signUp__input}
+            type="text"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          {/* <CheckIcon className={styles.signUp__inputIcon} /> */}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={styles.signUp__inputContainer}>
+          <input
+            className={styles.signUp__input}
+            type="text"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+          {/* <CheckIcon className={styles.signUp__inputIcon} /> */}
+        </div>
+        <div className={styles.signUp__btnSignContainer}>
+          <p className={styles.signUp__btn}>Login</p>
+          <Link href="/Home">
+            <button className={styles.signUp__btn}>Sign Up</button>
+          </Link>
+        </div>
+        <Link href="/Home">
+          <button className={styles.demo__btn}>Demo</button>
+        </Link>
+      </form>
     </div>
-  )
+  );
 }
