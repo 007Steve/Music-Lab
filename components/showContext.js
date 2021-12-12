d083e7ad89972b47bb9622e6abe7ba3aecd24eb9
007steve <stephen.plummer1.sp@gmail.com>
2021-12-12 14:18:46 -0500

    updated

---
+++ components/showContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+const ShowContext = React.createContext()
+const ShowProvider = ShowContext.Provider
+const ShowConsumer = ShowContext.Consumer
+export {ShowConsumer,ShowProvider}
