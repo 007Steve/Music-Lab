5d288df7236adf78ef5e7202cfb959ea1aa74f7b
007steve <stephen.plummer1.sp@gmail.com>
2021-12-12 13:51:50 -0500

    updated sidebar with icon and links

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,21 +1,69 @@
 import React from "react";
 import styles from "../styles/Sidebar.module.css";
 import SidebarCards from "./SidebarCards";
+import Link from "next/link";
+import Logo from "../components/Logo";
+import HomeIcon from "@material-ui/icons/Home";
+import PersonIcon from '@mui/icons-material/Person';
+import AlbumIcon from '@mui/icons-material/Album';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebar__container}>
-        <img className={styles.sidebar__logo} src="/logo.png" alt="" />
+        <div className={styles.sidebar__logo}>
+          <Logo />
+        </div>
       </div>
+
       <h4 className={styles.sidebar__myMuisc}>Menu</h4>
-      <SidebarCards texts="Home" />
-      <SidebarCards texts="Artist" />
-      <SidebarCards texts="Album" />
+     
+        <div className={styles.sidebar__sidebarCards}>
+        
+        <HomeIcon fontSize="small" className={styles.sidebar__icon} />
+        <Link href="/Home">
+        <h5 className={styles.sidebar__texts}>Home</h5>
+        </Link>
+      </div>
+     
+       <div className={styles.sidebar__sidebarCards}>
+        <PersonIcon fontSize="small" className={styles.sidebar__icon} />
+        
+        <Link href="/Artist">
+        <h5 className={styles.sidebar__texts}>Artist</h5>
+                </Link>
+      </div>
+       <div className={styles.sidebar__sidebarCards}>
+        <AlbumIcon fontSize="small" className={styles.sidebar__icon} />
+        <Link href="/Album">
+        <h5 className={styles.sidebar__texts}>Album</h5>
+                </Link>
+      </div>
+    
       <h4 className={styles.sidebar__myMuisc}>Playlist</h4>
-      <SidebarCards texts="Add Playlist" />
+        <div className={styles.sidebar__sidebarCards}>
+        <FavoriteIcon fontSize="small" className={styles.sidebar__icon} />
+        <Link href="/likes">
+        <h5 className={styles.sidebar__texts}>Liked Songs</h5>
+                </Link>
+      </div>
+      
       <h4 className={styles.sidebar__myMuisc}>Settings </h4>
-      <SidebarCards texts="Settings" />
-      <SidebarCards texts="Log Out" />
+       <div className={styles.sidebar__sidebarCards}>
+        <SettingsIcon fontSize="small" className={styles.sidebar__icon} />
+        <Link href="/Home">
+        <h5 className={styles.sidebar__texts}>Settings</h5>
+                </Link>
+      </div>
+      <div className={styles.sidebar__sidebarCards}>
+        <LogoutIcon fontSize="small" className={styles.sidebar__icon} />
+        <Link href="/">
+        <h5 className={styles.sidebar__texts}>Logout</h5>
+                </Link>
+      </div>
+     
     </div>
   );
 }
