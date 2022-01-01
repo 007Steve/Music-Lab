7564133cdb001c1091637ff88c1073c2db80588d
007steve <stephen.plummer1.sp@gmail.com>
2022-01-01 17:57:34 -0500

    fetch data and added redux

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,40 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../../styles/HomePage.module.css";
 import Nav from "../../components/Nav";
 import SongCard from "../../components/SongCard";
 import Sidebar from "../../components/Sidebar";
 import MusicController from "../../components/MusicController";
-function index() {
+import { useDispatch } from 'react-redux'
+import { songs } from '../../redux/songsSlice'
+function index({ data }) {
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(songs(data))
+  }, [])
   return (
     <div className={styles.homePage}>
       <Sidebar />
       <div className={styles.homePage__container}>
-        <Nav />
+        <Nav user={data} />
         <div className={styles.homePage__headerContainer}>
           <img
             className={styles.homePage__img}
-            src="https://cdn.vox-cdn.com/thumbor/b3sUwdyAUyusFlcsQWWzD1c9GaI=/0x112:493x441/1400x1400/filters:focal(0x112:493x441):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49601353/Screen_Shot_2016-05-01_at_12.14.34_PM.0.0.png"
+            src={data[0].cover}
             alt=""
           />
-          <h1 className={styles.homePage__title}>Title</h1>
+          <h1 className={styles.homePage__title}>{data[0].song}</h1>
         </div>
         <div className={styles.homePage__songsContainer}>
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
+
+
+          {data.map((data) => (
+            <SongCard title={data.song} img={data.cover} />
+          ))}
+
         </div>
       </div>
       <MusicController />
@@ -43,3 +41,12 @@ function index() {
 }
 
 export default index;
+
+/// fetch data
+export async function getStaticProps(context) {
+  const res = await fetch('http://localhost:3001/api/hello')
+  const data = await res.json()
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
