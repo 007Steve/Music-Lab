f86e17cc3a04b23bb50b6659e327a1fbdc71478a
007steve <stephen.plummer1.sp@gmail.com>
2022-01-02 09:13:02 -0500

    added current song from redux

--- components/MusicController.js
+++ components/MusicController.js
@@ -5,32 +5,35 @@ import SkipNextIcon from "@material-ui/icons/SkipNext";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import VolumeUpIcon from "@material-ui/icons/VolumeUp";
 import VolumeOffIcon from "@material-ui/icons/VolumeOff";
+import { useSelector } from "react-redux";
+import { selectcurrentSong } from '../redux/currentSongSlice'
 function MusicController() {
+  const currentSong = useSelector(selectcurrentSong)
   return (
     <div className={styles.musicController}>
       <div className={styles.musicController__infoLeftContainer}>
         <img
           className={styles.musicController__img}
-          src="https://cdn.vox-cdn.com/thumbor/b3sUwdyAUyusFlcsQWWzD1c9GaI=/0x112:493x441/1400x1400/filters:focal(0x112:493x441):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49601353/Screen_Shot_2016-05-01_at_12.14.34_PM.0.0.png"
+          src={currentSong.cover}
           alt=""
         />
         <div>
-          <h5>Title</h5>
+          <h5>{currentSong.song}</h5>
           <h6 className={styles.musicController__artist}>Artist</h6>
         </div>
       </div>
       <div className={styles.musicController__infoCenterContainer}>
-         <div className={styles.musicController__iconContainer}>
+        <div className={styles.musicController__iconContainer}>
           <SkipPreviousIcon className={styles.musicController__icon} />
           <PlayCircleFilledIcon
             fontSize="large"
             className={styles.musicController__icon}
           />
           <SkipNextIcon className={styles.musicController__icon} />
-        </div> 
+        </div>
 
         <div className={styles.musicController__controller}>
-          <p  className={styles.musicController__time}>2:00</p>
+          <p className={styles.musicController__time}>2:00</p>
           <input
             className={styles.musicController__input}
             min={0}
@@ -38,15 +41,15 @@ function MusicController() {
             value={50}
             type="range"
           />
-           <p  className={styles.musicController__time}>2:00</p>
+          <p className={styles.musicController__time}>2:00</p>
         </div>
       </div>
       <div className={styles.musicController__infoRightContainer}>
         {false ? (
           <VolumeUpIcon className={styles.musicController__icon} />
         ) : (
-          <VolumeOffIcon className={styles.musicController__icon} />
-        )}
+            <VolumeOffIcon className={styles.musicController__icon} />
+          )}
         {/* <input className={styles.musicController__inputVolume} min={0} max={100} value={50}  type="range" /> */}
       </div>
     </div>
