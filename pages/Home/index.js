c992448d1219745fa92c21a7dc6b2ca96616befa
007steve <stephen.plummer1.sp@gmail.com>
2022-01-02 09:18:25 -0500

    added current song and fetch data

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,18 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "../../styles/HomePage.module.css";
 import Nav from "../../components/Nav";
 import SongCard from "../../components/SongCard";
 import Sidebar from "../../components/Sidebar";
 import MusicController from "../../components/MusicController";
-import { useDispatch } from 'react-redux'
-import { songs } from '../../redux/songsSlice'
+import { useSelector } from "react-redux";
+import { selectcurrentSong } from '../../redux/currentSongSlice'
 function index({ data }) {
-  
-  const dispatch = useDispatch()
+  const currentSong = useSelector(selectcurrentSong)
 
-  useEffect(() => {
-    dispatch(songs(data))
-  }, [])
   return (
     <div className={styles.homePage}>
       <Sidebar />
@@ -21,18 +17,15 @@ function index({ data }) {
         <div className={styles.homePage__headerContainer}>
           <img
             className={styles.homePage__img}
-            src={data[0].cover}
-            alt=""
+            src={currentSong.cover}
+            alt="song cover"
           />
-          <h1 className={styles.homePage__title}>{data[0].song}</h1>
+          <h1 className={styles.homePage__title}>{currentSong.song}</h1>
         </div>
         <div className={styles.homePage__songsContainer}>
-
-
           {data.map((data) => (
-            <SongCard title={data.song} img={data.cover} />
+            <SongCard song={data} key={data.id} />
           ))}
-
         </div>
       </div>
       <MusicController />
@@ -47,6 +40,6 @@ export async function getStaticProps(context) {
   const res = await fetch('http://localhost:3001/api/hello')
   const data = await res.json()
   return {
-    props: { data }, // will be passed to the page component as props
+    props: { data },
   }
 }
