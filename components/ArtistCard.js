310154106d172e89090cf1c09d70955514381583
007steve <stephen.plummer1.sp@gmail.com>
2022-01-02 09:14:10 -0500

    added current song

--- components/ArtistCard.js
+++ components/ArtistCard.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "../styles/ArtistCard.module.css";
-function ArtistCard({name}) {
+function ArtistCard({ song,type}) {
+
   return (
     <div className={styles.artistCard}>
       <img
-        className={`${styles.artistCard__img} ${styles[name]} `}
-        src="https://cdn.vox-cdn.com/thumbor/b3sUwdyAUyusFlcsQWWzD1c9GaI=/0x112:493x441/1400x1400/filters:focal(0x112:493x441):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49601353/Screen_Shot_2016-05-01_at_12.14.34_PM.0.0.png"
-        alt=""
+        className={`${styles.artistCard__img} ${styles[type]} `}
+        src={song.cover}
+        alt="song cover"
       />
-      <h5 className={styles.artistCard__name}>Chance The Rapper</h5>
-      <p  className={styles.artistCard__song}>Coloring Book</p>
+      <h5 className={styles.artistCard__name}>{song.artist}</h5>
+      <p className={styles.artistCard__song}>{song.song}</p>
     </div>
   );
 }
